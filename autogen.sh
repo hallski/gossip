4efc9af515dbc169b9e32a596b52ca17a91eb5ba
Martyn James Russell <mr@src.gnome.org>
2006-02-27 01:09:01 +0000

    Tell the developer to download automake 1.9 instead of 1.7 when the wrong version or no version is installed

--- autogen.sh
+++ autogen.sh
@@ -53,7 +53,7 @@ DIE=0
 ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.7.tar.gz"
+	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.9.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
 }
