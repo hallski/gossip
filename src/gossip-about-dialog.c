f135f2847b045e8a7f38ec2c2385f409792577a4
Francisco Javier F. Serrador <serrador@openshine.com>
2006-07-18 12:27:39 +0000

    Updated FSF snail mail address.
    2006-07-18  Francisco Javier F. Serrador  <serrador@openshine.com>
    
    	*  src/gossip-about-dialog.c: Updated FSF snail mail address.

--- src/gossip-about-dialog.c
+++ src/gossip-about-dialog.c
@@ -69,7 +69,7 @@ const char *license[] = {
 	   "GNU General Public License for more details."),
 	N_("You should have received a copy of the GNU General Public License "
 	   "along with Gossip; if not, write to the Free Software Foundation, Inc., "
-	   "59 Temple Place, Suite 330, Boston, MA  02111-1307  USA")
+	   "51 Franklin Street, Fifth Floor, Boston, MA 02110-130159 USA")
 };
 
 static void
