6631147edc74150323886aa8f577b06e7a989e0d
Mikael Hallendal <micke@imendio.com>
2007-05-26 00:34:22 +0000

    Added dummy object.
    2007-05-24  Mikael Hallendal  <micke@imendio.com>
    
    	* src/Makefile.am: Fixed the style a bit and added GossipDummy.
    	* src/gossip-dummy.[ch]: Dummy object for boilerplate.
    
    svn path=/trunk/; revision=2349

---
+++ src/gossip-dummy.c
@@ -0,0 +1,129 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2007 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include "gossip-dummy.h"
+
+#define GET_PRIV(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), GOSSIP_TYPE_DUMMY, GossipDummyPriv))
+
+typedef struct _GossipDummyPriv GossipDummyPriv;
+
+struct _GossipDummyPriv {
+	gint my_prop;
+};
+
+static void         dummy_finalize           (GObject             *object);
+static void         dummy_get_property       (GObject             *object,
+					      guint                param_id,
+					      GValue              *value,
+					      GParamSpec          *pspec);
+static void         dummy_set_property       (GObject             *object,
+					      guint                param_id,
+					      const GValue        *value,
+					      GParamSpec          *pspec);
+
+enum {
+	PROP_0,
+	PROP_MY_PROP
+};
+
+G_DEFINE_TYPE (GossipDummy, gossip_dummy, G_TYPE_OBJECT);
+
+static void
+gossip_dummy_class_init (GossipDummyClass *class)
+{
+	GObjectClass *object_class;
+
+	object_class = G_OBJECT_CLASS (class);
+
+	object_class->finalize     = dummy_finalize;
+	object_class->get_property = dummy_get_property;
+	object_class->set_property = dummy_set_property;
+
+	g_object_class_install_property (object_class,
+					 PROP_MY_PROP,
+					 g_param_spec_int ("my-prop",
+							   "",
+							   "",
+							   0, 1,
+							   1,
+							   G_PARAM_READWRITE));
+
+	g_type_class_add_private (object_class, sizeof (GossipDummyPriv));
+}
+
+static void
+gossip_dummy_init (GossipDummy *presence)
+{
+	GossipDummyPriv *priv;
+
+	priv = GET_PRIV (presence);
+}
+
+static void
+dummy_finalize (GObject *object)
+{
+	GossipDummyPriv *priv;
+
+	priv = GET_PRIV (object);
+
+	(G_OBJECT_CLASS (gossip_dummy_parent_class)->finalize) (object);
+}
+
+static void
+dummy_get_property (GObject    *object,
+		    guint       param_id,
+		    GValue     *value,
+		    GParamSpec *pspec)
+{
+	GossipDummyPriv *priv;
+
+	priv = GET_PRIV (object);
+
+	switch (param_id) {
+	case PROP_MY_PROP:
+		g_value_set_int (value, priv->my_prop);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
+		break;
+	}
+}
+static void
+dummy_set_property (GObject      *object,
+		    guint         param_id,
+		    const GValue *value,
+		    GParamSpec   *pspec)
+{
+	GossipDummyPriv *priv;
+
+	priv = GET_PRIV (object);
+
+	switch (param_id) {
+	case PROP_MY_PROP:
+		priv->my_prop = g_value_get_int (value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
+		break;
+	}
+}
+
---
+++ src/gossip-dummy.h
@@ -0,0 +1,51 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2007 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GOSSIP_DUMMY_H__
+#define __GOSSIP_DUMMY_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define GOSSIP_TYPE_DUMMY            (gossip_dummy_get_type ())
+#define GOSSIP_DUMMY(o)              (G_TYPE_CHECK_INSTANCE_CAST ((o), GOSSIP_TYPE_DUMMY, GossipDummy))
+#define GOSSIP_DUMMY_CLASS(k)        (G_TYPE_CHECK_CLASS_CAST ((k), GOSSIP_TYPE_DUMMY, GossipDummyClass))
+#define GOSSIP_IS_PRESENCE(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GOSSIP_TYPE_DUMMY))
+#define GOSSIP_IS_PRESENCE_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GOSSIP_TYPE_DUMMY))
+#define GOSSIP_DUMMY_GET_CLASS(o)    (G_TYPE_INSTANCE_GET_CLASS ((o), GOSSIP_TYPE_DUMMY, GossipDummyClass))
+
+typedef struct _GossipDummy      GossipDummy;
+typedef struct _GossipDummyClass GossipDummyClass;
+
+struct _GossipDummy {
+	GObject parent;
+};
+
+struct _GossipDummyClass {
+	GObjectClass parent_class;
+};
+
+GType               gossip_dummy_get_type                 (void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* __GOSSIP_DUMMY_H__ */
+
