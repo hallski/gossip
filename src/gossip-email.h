a76afb1204058b9714bd29c6c239d3cab913a04c
Mikael Hallendal <hallski@src.gnome.org>
2007-05-30 13:33:13 +0000

    Added missing files.
    svn path=/trunk/; revision=2428

---
+++ src/gossip-email.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2007 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GOSSIP_EMAIL_H__
+#define __GOSSIP_EMAIL_H__
+
+#include <glib.h>
+
+#include <libgossip/gossip-contact.h>
+
+G_BEGIN_DECLS
+
+gboolean            gossip_email_available (GossipContact *contact);
+void                gossip_email_open      (GossipContact *contact);
+
+G_END_DECLS
+
+#endif /* __GOSSIP_EMAIL_H__ */
+
