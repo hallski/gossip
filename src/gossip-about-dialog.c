7c6abb36809c9f3d0dc92804896f704787db0f3b
Martyn James Russell <mr@src.gnome.org>
2007-05-31 00:36:48 +0000

    Remove Telepathy "backend" code we added here to identify the backend we
    	* src/gossip-about-dialog.c: (gossip_about_dialog_new): Remove
    	Telepathy "backend" code we added here to identify the backend we
    	were using.
    
    svn path=/trunk/; revision=2446

--- src/gossip-about-dialog.c
+++ src/gossip-about-dialog.c
@@ -84,10 +84,7 @@ about_dialog_activate_link_cb (GtkAboutDialog *about,
 void
 gossip_about_dialog_new (GtkWindow *parent)
 {
-	gchar       *license_trans;
-	gchar       *comments;
-	gchar       *technology;
-	const gchar *backend;
+	gchar *license_trans;
 
 	gtk_about_dialog_set_url_hook (about_dialog_activate_link_cb, NULL, NULL);
 
@@ -96,22 +93,10 @@ gossip_about_dialog_new (GtkWindow *parent)
 				     _(license[2]), "\n\n",
 				     NULL);
 
-#ifdef HAVE_TELEPATHY
-	backend = "Telepathy";
-#else
-	backend = "Jabber";
-#endif
-	
-	technology = g_strdup_printf (_("Using the %s backend"), backend);
-	comments = g_strdup_printf ("%s\n%s",
-				    _("An Instant Messaging client for GNOME"),
-	                            technology);
-	g_free (technology);
-
 	gtk_show_about_dialog (parent,
 			       "artists", artists,
 			       "authors", authors,
-			       "comments", comments,
+			       "comments", _("An Instant Messaging client for GNOME"),
 			       "license", license_trans,
 			       "wrap-license", TRUE,
 			       "copyright", "Imendio AB 2002-2007",
@@ -123,7 +108,6 @@ gossip_about_dialog_new (GtkWindow *parent)
 			       NULL);
 
 	g_free (license_trans);
-	g_free (comments);
 }
 
 
